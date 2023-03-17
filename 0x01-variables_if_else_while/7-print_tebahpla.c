d21a2ddba235db882804f62c5c2f362cf22af2c0
Eyram Cobblah <ecobblah2@gmail.com>
2023-03-17 22:04:54 +0000

    Added print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print lowercase alphabet in reverse followed by new line.
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
