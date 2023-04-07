107f7eea774f7ff01843ebf939e1d4ac6b8f2a3a
Eyram Cobblah <ecobblah2@gmail.com>
2023-04-07 20:40:25 +0000

    Corrections libmy.a

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * File: main
- *
- *Desc: header file
- */
+#ifndef _MAIN_H
+#define _MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
